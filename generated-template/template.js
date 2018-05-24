e975aff8136485b254b69dc8d850f5f717ab4393
Coder-ACJHP <hexa.octabin@gmail.com>
2018-05-24 22:41:50 +0200

    Added new elements

---
+++ generated-template/template.js
@@ -0,0 +1,20 @@
+let isShown = false;
+let openedLetterIcon = "far fa-envelope-open";
+let envelopedLetterIcon = "fas fa-envelope";
+let trigger = document.getElementById('trigger');
+
+trigger.addEventListener('click', () => {
+
+    if (isShown) {
+        trigger.setAttribute('class', envelopedLetterIcon);
+        document.getElementById('popover').style.display = 'none';
+        document.querySelector('.popover-arrow').style.display = 'none';
+        isShown = false;
+    } else {
+        trigger.setAttribute('class', openedLetterIcon);
+        document.getElementById('popover').style.display = 'block'
+        document.querySelector('.popover-arrow').style.display = 'block';
+        isShown = true;
+    }
+
+});
